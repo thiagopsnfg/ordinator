2b22fc4591a357dd0303f57cbf6f27526d713961
Thiago P. Santos <thiagopsnfg@gmail.com>
2024-10-15 10:04:12 -0300

    prevent linter to adjust sample.js file

--- sample.js
+++ sample.js
@@ -1,9 +1,20 @@
+/* eslint-disable */
 import { lib3 } from "@pachage/other/lib3";
 import another_dummy_pkg from "@pachage/other/lib5";
 import simple from "@pachage/other/lib0";
 import lib1 from "@pachage/other/lib1";
 import dummy_pkg from "@pachage/other/lib4";
 
-const { dummy_pkg, lib3, another_dummy_pkg, simple } = require("some/module");
+const { 
+  dummy_pkg_01,
+  lib6,
+  another_dummy_pkg_6,
+  simple_01
+} = require("some/module");
 
-const FIELDS = ["dummy_pkg", "lib3", "another_dummy_pkg", "simple"];
+const FIELDS = [
+  "dummy_pkg",
+  "lib3",
+  "another_dummy_pkg",
+  "simple"
+];
